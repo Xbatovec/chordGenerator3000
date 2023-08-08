11f58b83743e51b6fae42e67b18e4c3c98748b3d
Xbat <99105387+Xbatovec@users.noreply.github.com>
2023-08-08 11:30:07 +0200

    Chord update

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function elId(id) {return document.getElementById(id);}
 function elClass(id) {return document.getElementsByClassName(id);}
 function elTag(id) {return document.getElementsByTagName(id)[0];}
 
-const cord = elId("cord");
-const cordMap = elId("cord-map");
-const ctx = cordMap.getContext('2d');
+const chord = elId("cord");
+const chordMap = elId("cord-map");
+const ctx = chordMap.getContext('2d');
